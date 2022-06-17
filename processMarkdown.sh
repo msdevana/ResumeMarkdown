13579d86348d9b7151e0f36af8bf87cac67ff94d
Manish S. Devana <mdevana@miami.edu>
2022-06-17 13:10:52 -0400

    update CV

--- processMarkdown.sh
+++ processMarkdown.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "Processing Markdown into PDF Resume: Happy Hunting!"
-
+## stage all commits and commit them first if you want (must have git set up)
+git stage --all
+git commit -m "update CV"
+git push
 pandoc resume.md -f markdown -t pdf --pdf-engine=wkhtmltopdf -c resume-stylesheet.css -s -o resume.pdf
